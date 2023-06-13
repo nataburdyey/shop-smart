42819df977ef87e7948c8880a8c6c13af8d2e6c8
Natalia Burdyey <nata.burdey@gmail.com>
2023-06-13 19:20:35 -0400

    ShopSmart complete

---
+++ src/Form.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) {
+      toast.error("Please provide value");
+      return;
+    }
+    addItem(newItemName);
+    setNewItemName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>ShopSmart</h4>
+      <div className="form-control">
+        <input
+          type="text"
+          className="form-input"
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Add Item
+        </button>
+      </div>
+    </form>
+  );
+};
+export default Form;
